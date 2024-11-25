8e64991a38c24a7d4b1b0783a2e12844fb5d15ad
yy-gx <yyang9923@gmail.com>
2024-11-25 17:49:01 -0500

    working on diffusion policy

--- lerobot/scripts/evaluate_on_ori_task_new.py
+++ lerobot/scripts/evaluate_on_ori_task_new.py
@@ -114,6 +114,7 @@ def parse_args():
                  "modified_libero", "single_step"],
         default="libero_90"
     )
+    parser.add_argument("--folder_name", type=str, default="act_4")
     parser.add_argument("--task_order_index", type=int, default=5)
     parser.add_argument("--task_num_to_use", type=int, default=20)
     parser.add_argument("--seed", type=int, required=True, default=10000)
@@ -161,7 +162,7 @@ def main():
         cfg.bddl_folder = get_libero_path("bddl_files")
         cfg.init_states_folder = get_libero_path("init_states")
         cfg.device = args.device_id
-        save_dir = os.path.join("outputs/libero_eval", f"eval_tasks_on_ori_envs_diffusion_task_order_index_{args.task_order_index}_seed{args.seed}", f"evaluation_task{task_id}_on_ori_envs")
+        save_dir = os.path.join("outputs/libero_eval", f"eval_tasks_on_ori_envs_diffusion_{args.folder_name}_seed{args.seed}", f"evaluation_task{task_id}_on_ori_envs")
         print(f">> Create folder {save_dir}")
         os.system(f"mkdir -p {save_dir}")
 
@@ -285,10 +286,10 @@ def main():
 
             succ_list.append(success_rate)
             torch.save(eval_stats, save_stats_pth)
-            with open(os.path.join("outputs/libero_eval", f"eval_tasks_on_ori_envs_diffusion_task_order_index_{args.task_order_index}_seed{args.seed}", f"succ_list_evaluation_on_ori_envs.npy"), 'wb') as f:
+            with open(os.path.join("outputs/libero_eval", f"eval_tasks_on_ori_envs_diffusion_{args.folder_name}_seed{args.seed}", f"succ_list_evaluation_on_ori_envs.npy"), 'wb') as f:
                 np.save(f, np.array(succ_list))
 
-        with open(os.path.join("outputs/libero_eval", f"eval_tasks_on_ori_envs_diffusion_task_order_index_{args.task_order_index}_seed{args.seed}", f"succ_list_evaluation_on_ori_envs.npy"), 'wb') as f:
+        with open(os.path.join("outputs/libero_eval", f"eval_tasks_on_ori_envs_diffusion_{args.folder_name}_seed{args.seed}", f"succ_list_evaluation_on_ori_envs.npy"), 'wb') as f:
             np.save(f, np.array(succ_list))
         print(
             f"[info] finish for ckpt at {model_path} in {t.get_elapsed_time()} sec for rollouts"
